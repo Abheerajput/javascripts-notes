c453907d7e641635de19dabbe276f9562651d0b9
Abheerajput <127973979+Abheerajput@users.noreply.github.com>
2023-10-05 15:20:52 +0000

    main

---
+++ datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all js code as newr version
+//alert(3+7)//we are using node js, not browser
+
+console.log("abhishek")
+
+    //  datatypes 
+let name = "abhishek" 
+//string =>  first="" ,second=''
+
+
+
+let length =25 
+//number => 2 tp power 53  
+//two types  1)without decimal 54
+// 2) with decimals 43.53
+
+
+// exponential notation
+// 1.=> extra large number 
+let y=1626e4;
+//2=> extra small number
+let z=123e-5;
+
+let isLoggedIn = false // booleans   flase/true
+let  x; // undefined
+
+const cars =["toyta","bmw","audi"]; //array datatypes

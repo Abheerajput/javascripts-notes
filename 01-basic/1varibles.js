908093357dd11ba02ff241fe5417861701634579
Abheerajput <127973979+Abheerajput@users.noreply.github.com>
2023-10-05 18:07:25 +0530

    Update 1varibles.js

--- 01-basic/1varibles.js
+++ 01-basic/1varibles.js
@@ -4,9 +4,11 @@ var accountPassword ="287352"
 accountCity ="hisar"
 
 // accountId =3
+// prefer not to use var because of issue in block scope and 
+// functional scope
+
 accountPassword="67474767 "
 accountEmail ="avsnb@gmail.com"
-
 accountCity ="delhi"
 console.log(accountId);
 console.table([accountEmail,accountId,accountPassword,accountCity])

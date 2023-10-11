54cc523e026c40851cb363707ad15d370111cb21
Abheerajput <127973979+Abheerajput@users.noreply.github.com>
2023-10-11 12:31:41 +0530

    Add files via upload

---
+++ destructure.js
@@ -0,0 +1,8 @@
+const course ={
+    coursename:"full stack developer",
+    duration:"1year",
+    courseprice:"100000",
+    location:"hisar"
+}
+const {courseprice: price} = course
+console.log(price)
